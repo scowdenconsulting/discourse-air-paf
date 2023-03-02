522f3b53c4bb966b1703b0fe1305673dcacc66a2
Scott Scowden <scott@graydogsolutions.com>
2023-03-01 22:56:42 -0500

    Test https

--- javascripts/discourse/initializers/test.js
+++ javascripts/discourse/initializers/test.js
@@ -7,7 +7,28 @@ export default {
         withPluginApi('0.8', api => {
             api.onPageChange(x => {
                 if (x == "/login") {
-                    fetch("http://ip.jsontest.com/")
+                    //var xmlhttp = new XMLHttpRequest();
+                    //var url = "http://ip.jsontest.com/";
+
+                    //xmlhttp.onreadystatechange = function () {
+                    //    if (this.readyState == 4 && this.status == 200) {
+                    //        var myArr = JSON.parse(this.responseText);
+                    //        myFunction(myArr);
+                    //    }
+                    //};
+                    //xmlhttp.open("GET", url, true);
+                    //xmlhttp.send();
+
+                    //function myFunction(arr) {
+                    //    var out = "";
+                    //    var i;
+                    //    for (i = 0; i < arr.length; i++) {
+                    //        out += '<a href="' + arr[i].url + '">' +
+                    //            arr[i].display + '</a><br>';
+                    //    }
+                    //    document.getElementById("id01").innerHTML = out;
+                    //}
+                    fetch("https://raw.githubusercontent.com/json-iterator/test-data/master/large-file.json")
                         .then((response) => response.json())
                         .then((data) => console.log(data));
                 }
